28701fe0db5df147609234e3f772f00d88592b87
Antoine Catton <devel@antoine.catton.fr>
2015-07-19 00:05:04 -0600

    Fix #2

--- src/Main.hs
+++ src/Main.hs
@@ -15,18 +15,43 @@
 
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitWith, ExitCode(ExitSuccess, ExitFailure))
-import Data.Yaml (decodeFile, Value(Object))
+import qualified Data.Yaml as Yaml
 import qualified Data.List as List
+import qualified Data.Either as Either
+import qualified Data.HashMap.Lazy as HashMap
+import Control.Monad (foldM)
+import Data.Text (pack)
 
 import qualified YamlKeysDiff.Opts as Opts
+import YamlKeysDiff.Filename (parseFileName)
 import YamlKeysDiff.Diff (diff)
 
-decodeFile' :: FilePath -> IO Value
-decodeFile' path = do
-    maybeContent <- decodeFile path
-    case maybeContent of
-        Just content -> return content
-        Nothing -> ioError $ userError $ "couldn't decode file " ++ path
+decodeFilename :: String -> IO (FilePath, [String])
+decodeFilename fileName =
+    case parseFileName fileName of
+        Either.Right a -> return a
+        Either.Left e ->
+            ioError $ userError $ "couldn't parse filename " ++ fileName
+
+
+decodeFile :: FilePath -> IO Yaml.Value
+decodeFile path = do
+    (fileName, keys) <- decodeFilename path
+    maybeContent <- Yaml.decodeFile fileName
+    content <- case maybeContent of
+        Just c -> return c
+        Nothing -> ioError $ userError $ "couldn't decode file " ++ fileName
+    case getValue keys content of
+        Just v -> return v
+        Nothing -> ioError $ userError $ "couldn't find the key " ++ List.intercalate ":" keys
+
+getValue :: [String] -> Yaml.Value -> Maybe Yaml.Value
+getValue keys value =
+    let getKey key value =
+            case value of
+                Yaml.Object obj -> HashMap.lookup key obj
+                _ -> Nothing
+    in foldM (flip getKey) value $ map pack keys
 
 getFiles :: [String] -> IO (String, String)
 getFiles args =
@@ -44,8 +69,8 @@ main = do
     else do
         formattingFunction <- Opts.getFormattingFunction flags
         (filePathA, filePathB) <- getFiles args
-        contentA <- decodeFile' filePathA
-        contentB <- decodeFile' filePathB
+        contentA <- decodeFile filePathA
+        contentB <- decodeFile filePathB
         let diffLines = diff contentA contentB
         putStr $ formattingFunction diffLines
         -- FIXME: ioError also exit with 1
